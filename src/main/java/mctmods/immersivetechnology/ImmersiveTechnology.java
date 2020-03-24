dcf68a62bbc5b7d752dda0b71430c269a9486e21
tgstyle <tg.style@gmail.com>
2020-03-23 21:16:58 -0600

    It loads
    but does nothing else.

--- src/main/java/mctmods/immersivetechnology/ImmersiveTechnology.java
+++ src/main/java/mctmods/immersivetechnology/ImmersiveTechnology.java
@@ -1,21 +1,21 @@
 package mctmods.immersivetechnology;
 
-import blusunrize.immersiveengineering.common.Config;
+//import blusunrize.immersiveengineering.common.Config;
 import mctmods.immersivetechnology.common.CommonProxy;
 import mctmods.immersivetechnology.common.ITContent;
 import mctmods.immersivetechnology.common.util.ITLogger;
 import mctmods.immersivetechnology.common.util.ITSounds;
 import mctmods.immersivetechnology.common.util.compat.ITCompatModule;
-import net.minecraft.creativetab.CreativeTabs;
+//import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidRegistry;
+//import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.*;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+//import net.minecraftforge.fml.common.Mod.EventHandler;
+//import net.minecraftforge.fml.common.Mod.Instance;
+//import net.minecraftforge.fml.common.SidedProxy;
+//import net.minecraftforge.fml.common.event.*;
+//import net.minecraftforge.fml.common.network.NetworkRegistry;
+//import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
 @Mod(ImmersiveTechnology.MODID)
 public class ImmersiveTechnology {
@@ -23,7 +23,9 @@ public class ImmersiveTechnology {
 	public static final String MODID = "immersivetechnology";
 	public static final String NAME = "Immersive Technology";
 	public static final String VERSION = "${version}";
-
+	
+}
+/*
 	@SidedProxy(clientSide = "mctmods.immersivetechnology.client.ClientProxy" , serverSide = "mctmods.immersivetechnology.common.CommonProxy")
 	public static CommonProxy proxy;
 	public static final SimpleNetworkWrapper packetHandler = NetworkRegistry.INSTANCE.newSimpleChannel(MODID);
@@ -70,4 +72,4 @@ public class ImmersiveTechnology {
 		}
 	};
 
-}
+}*/
